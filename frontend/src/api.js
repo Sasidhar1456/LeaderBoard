e222b68bf35d682ccc9b80feb34a4422b9f8c076
Sasidhar1456 <sasidharpinjala@gmail.com>
2025-07-17 17:23:37 +0530

    sasi1

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://leaderboard-9yrz.onrender.com/api' });
 
 export const getUsers = () => API.get('/users');
 export const addUser = (name) => API.post('/users', { name });
